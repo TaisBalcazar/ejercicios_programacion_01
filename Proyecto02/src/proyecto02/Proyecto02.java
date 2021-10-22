114bc186abd1ea6b06a45210f05bbb15d7f9a9c8
John Doe <johndoe@example.com>
2021-10-22 11:02:12 -0500

    primer commit

---
+++ Proyecto02/src/proyecto02/Proyecto02.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto02;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Proyecto02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Generando otro miniprograma en Java");
+        System.out.println("Usando Netbeans, Git y GitHub");
+        
+
+        // TODO code application logic here
+    }
+    
+}
